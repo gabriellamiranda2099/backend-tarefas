27e9688a2a80bde09ea258c27403348c8d36bfb0
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-03-30 13:51:04 +0000

    versao2

--- cnh.js
+++ cnh.js
@@ -2,3 +2,12 @@ const idade = 15;
 const maiorIdade = idade >= 18;
 const possuiCNH = false;
 
+const podeDirigir = maiorIdade && possuiCNH;
+console.log(`Pode viaar sozinho?`, podeDirigir);
+
+const podeViajarSozinho = maiorIdade || possuiCNH; 
+console.log (`Pode viajar sozinho?`, podeViajarSozinho);
+
+const precisaDeAcompanhante = !maiorIdade; 
+console.log(`Precisa de acompanhante?`, precisaDeAcompanhante);
+
