84e5a8b41126a06d813d663f35a8eb9cff01f816
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-04-07 11:51:32 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,11 @@
+//Sistema de Pontuação de um jogo
+const nomeJogador = "Geto"
+let faseAtual = 3;
+let vidasRestantes =2;
+
+let pontosPorMissao = [85, 90, 60, 100];
+
+//pontosPorMissao.lenght
+
+console.log(`Seja bem vindo: ${nomeJogador}!`);
+console.log(pontosPorMissao);
