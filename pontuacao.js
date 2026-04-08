d1c0a8504417f49c515dc71614291f62a45b71a2
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-04-08 12:30:31 +0000

    versao2

--- pontuacao.js
+++ pontuacao.js
@@ -6,6 +6,15 @@ let vidasRestantes =2;
 let pontosPorMissao = [85, 90, 60, 100];
 
 //pontosPorMissao.lenght
+let pontuacaoTotal =0;
+for (let i =0; < pontuacoes.lenght; i++) {
+    pontuacaoTotal= pontuacaoTotal + pontuacoes[i]
+}
+//Calculando a média
+let media = pontuacaoTotal / pontuacoes.lenght;
+
+let passouDeFase = (media >= 75 && vidas > 0) || (fase === 1)
 
 console.log(`Seja bem vindo: ${nomeJogador}!`);
 console.log(pontosPorMissao);
+
