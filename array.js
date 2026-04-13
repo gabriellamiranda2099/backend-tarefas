6c0b69b6813b28784a6042bbf85df22ea99e9663
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-04-13 11:50:49 +0000

    V1

---
+++ array.js
@@ -0,0 +1,12 @@
+//Lista de frutas- ARRAY
+
+const frutas = ["Maçã", "Banana", "Uva","Manga"];
+
+console.log(frutas[1]);
+console.log(frutas[3]);
+console.log(frutas.lenght);
+frutas[1] = "Morango"; //Altera o valor do elemento
+frutas.push("Pessego"); //Adiciona um item elemento no final
+frutas.unishift("Pera");
+console.log(frutas);
+console.log(frutas.lenght);
