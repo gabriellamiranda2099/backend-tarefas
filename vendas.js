c72d3baf773183a6b4fb129194576dabd08c6d7b
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-04-28 11:35:12 +0000

    versao2

--- vendas.js
+++ vendas.js
@@ -13,4 +13,15 @@ let estoqueOK = quantidadePedido <= estoque;
 
 if (!estoqueOK) {
     console.log("Pedido RECUSADO - estoque insuficiente")
-}
+}console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + "," + numero + "-" + cidade;
+
+console.log(enderecoCompleto);
+
+//Adicione informações adicionais
